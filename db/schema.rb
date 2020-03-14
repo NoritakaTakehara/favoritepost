6eb5da75da22c6c33c6794fea17b2b7b06fbe9f3
NoritakaTakehara <50361226+NoritakaTakehara@users.noreply.github.com>
2020-03-14 20:21:57 +0900

    nicknameカラムに一意性制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_105229) do
     t.datetime "updated_at", null: false
     t.text "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
